b3dbaf512aa02a05009b61bccb29624308e23b12
Jay-Thomas-code <rubat0@naver.com>
2021-07-04 23:06:09 +0900

    error case : 4

--- ETOOS/etoos.js
+++ ETOOS/etoos.js
@@ -13,8 +13,9 @@
                 // (node:39577) UnhandledPromiseRejectionWarning: Error: No node found for selector: #records_form > div > img:nth-child(5)
                 // 미확인
 
+                // 4. TimeoutError: waiting for selector `#ui-datepicker-div > table > tbody > tr:nth-child(2) > td:nth-child(4) > a` failed: timeout 30000ms exceeded
 
-
+                // 5. 주차별 1page 만 탐색하고 다음 주차로 넘어가는 오류 발생
 
 const puppeteer = require('puppeteer');
 
@@ -24,7 +25,6 @@ const puppeteer = require('puppeteer');
 
         headless : false,
         defaultViewport : null,
-        
     });
 
     const page = await browser.newPage();
@@ -43,15 +43,19 @@ const puppeteer = require('puppeteer');
                     return tds.map(td => td.innerText);
             });
                 console.log(data)
-        
+                await page.waitForTimeout(1000);
+                // if(await page.$('#container > div.contents > div.btn_area.mgt_15 > div > a:nth-child('+ i +')') === null) break;
+
+            // 10페이지 Table 추출 후 11페이지로 넘어감
             if(i===11){
-                // 10페이지 Table 추출 후 11페이지로 넘어감
+                
                 await page.waitForTimeout(1000);
                 await page.waitForSelector('#container > div.contents > div.btn_area.mgt_15 > div > a.btn_next');
                 await page.click('#container > div.contents > div.btn_area.mgt_15 > div > a.btn_next');
                 
                 // page 11 ~ 20 Table 추출
                 for(let j=3; j<13; j++){ 
+                    page.waitForNavigation;
 
                     await page.waitForSelector('#container > div.contents > div.btn_area.mgt_15 > div > a:nth-child('+ j +')');
                     await page.click('#container > div.contents > div.btn_area.mgt_15 > div > a:nth-child('+ j +')');
@@ -61,21 +65,13 @@ const puppeteer = require('puppeteer');
                     const data = await page.evaluate(()=>{
                         const tds = Array.from(document.querySelectorAll('#container > div.contents > div.wrap_tbl_sdw.mgt_30')); 
                         return tds.map(td => td.innerText);    
-                });
-                    console.log(data)
-
-                for(let k=3; k<13; k++){
-                    await page.evaluate(()=>{
-                        if(document.querySelector(`#ui-datepicker-div > table > tbody > tr:nth-child(2) > td:nth-child(4) > a`) === null) break;
                     });
+                    console.log(data);
+                    await page.waitForTimeout(1000);
+                    if(await page.$('#container > div.contents > div.btn_area.mgt_15 > div > a:nth-child('+ j +')') === null) break;
                 };
-
-                    
-
-                }
-                
-            }                                                                                                                                                               
-        }
+            };                                                                                                                                                               
+        };
         })();
     };
 
@@ -83,23 +79,28 @@ const puppeteer = require('puppeteer');
         // 달력 6/1 이동
         (async() => {
 
-            page.waitForNavigation;
+            await page.waitForNavigation;
 
             for(let x=1; x<=4; x++){
                 // 달력 앞, 뒤 버튼, 검색 로딩
-                await page.waitForSelector('#records_form > div > img:nth-child(3)');
+                
                 await page.waitForSelector('#records_form > div > img:nth-child(5)');
                 await page.waitForSelector('#btn_search');
-                // 달력 앞 버튼 클릭                
+                // 달력 앞 버튼 클릭
+                await page.waitForNavigation;
+                await page.waitForTimeout(4000);
+                await page.waitForSelector('#records_form > div > img:nth-child(3)');
                 await page.click('#records_form > div > img:nth-child(3)');
-                await page.waitForTimeout(1000);
+                await page.waitForTimeout(4000);
                 // 시작 일 지정
+
                 await page.waitForSelector('#ui-datepicker-div > table > tbody > tr:nth-child('+ x +') > td:nth-child('+ (x+2) +') > a');    
+                await page.waitForTimeout(2000);
                 await page.click('#ui-datepicker-div > table > tbody > tr:nth-child('+ x +') > td:nth-child('+ (x+2) +') > a');
-                await page.waitForTimeout(1000);
+                await page.waitForTimeout(2000);
                 // 달력 뒤 버튼 클릭
                 await page.click('#records_form > div > img:nth-child(5)');
-                await page.waitForTimeout(1000);
+                await page.waitForTimeout(2000);
                 // 끝나는 일 지정
                 await page.waitForSelector('#ui-datepicker-div > table > tbody > tr:nth-child('+ (x+1) +') > td:nth-child('+ (x+2) +') > a');    
                 await page.click('#ui-datepicker-div > table > tbody > tr:nth-child('+ (x+1) +') > td:nth-child('+ (x+2) +') > a');
@@ -110,11 +111,12 @@ const puppeteer = require('puppeteer');
 
                 await extract_Table();
 
+                await page.waitForTimeout(1000);
 
             }
         })();
     };    
-        
+
     await page.goto('https://www.etoos.com/member/login.asp?returnUrl=http://247.etoos.com/lms/index.do');  
     page.waitForNavigation;
     await page.waitForTimeout(1000);
